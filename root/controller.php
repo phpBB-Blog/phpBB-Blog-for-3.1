6b5d78e46b648a7ee65cdc0125312c9320b8dd61
David King <imkingdavid@gmail.com>
2012-02-08 01:07:41 -0500

    try again

--- root/controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class phpbb_ext_blog_controller implements phpbb_extension_controller_interface
-{
-	public function handle()
-	{
-		// blog front page stuff
-	}
-}
--- root/ext.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class phpbb_ext_blog_ext extends phpbb_extension_base
-{
-	// extension stuff
-	// not entirely sure what all actually goes here
-	// it can really be left empty for now
-}
