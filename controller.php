69a7d5d90d45626aa2641ac4d09018e085c68f68
David King <imkingdavid@gmail.com>
2012-02-08 00:39:49 -0500

    Initial extension set up

---
+++ controller.php
@@ -0,0 +1,9 @@
+<?php
+
+class phpbb_ext_blog_controller implements phpbb_extension_controller_interface
+{
+	public function handle()
+	{
+		// blog front page stuff
+	}
+}
---
+++ ext.php
@@ -0,0 +1,8 @@
+<?php
+
+class phpbb_ext_blog_ext extends phpbb_extension_base
+{
+	// extension stuff
+	// not entirely sure what all actually goes here
+	// it can really be left empty for now
+}
