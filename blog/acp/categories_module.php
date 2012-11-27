bfe5940d0d32f167d3629880fb9248e312dfe926
Erik Frèrejean <erikfrerejean@phpbb.com>
2012-11-27 14:11:55 +0100

    Add ACP template file
    Make sure there is an ACP template file so that phpBB doesn't throw
    an error when accessing the page.

--- blog/acp/categories_module.php
+++ blog/acp/categories_module.php
@@ -20,10 +20,18 @@ if (!defined('IN_PHPBB'))
  */
 class phpbb_ext_phpbbblog_acp_categories_module
 {
+	/** @var string */
 	public $u_action;
+
+	/** @var string */
+	public $tpl_name;
+
+	/** @var string */
+	public $page_title;
 
 	public function main($id, $mode)
 	{
-	
+		$this->tpl_name		= 'acp_blog_categories';
+		$this->page_title	= '';
 	}
 }
