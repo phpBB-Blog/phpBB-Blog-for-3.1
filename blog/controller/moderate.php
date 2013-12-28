4d396355d93be2b7fd924d60f4cb7dc13d1b0c8e
MichaelC <m@michaelcullum.com>
2013-12-28 20:08:52 +0000

    Base controllers

---
+++ blog/controller/moderate.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *
+ * @package phpBB Blog
+ * @copyright (c) 2013 phpBB Blog Group
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+namespace phpbb_blog\blog\controller;
+
+class moderate
+{
+	protected $request;
+	protected $display;
+
+	/**
+	* Constructor
+	*
+	* @param \phpbb\request\request 		$request  	Request Object
+	* @param \phpbb_blog\blog\display 	$display 	Display Helper Object
+	*/
+	public function __construct(\phpbb\request\request $request, \phpbb_blog\blog\display $display)
+	{
+		$this->request = $request;
+		$this->display = $display;
+	}
+}
